b1991b1fefe209eeb0512e028bfcdf2bc493ca4a
jiayao <jiayao3.14@outlook.com>
2019-04-15 11:19:03 +0800

    创建仓库

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PAT
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(System.Console.ReadLine());
+            Console.WriteLine();
+        }
+        // static void Main(string[] args)
+        // {
+        //     int n = Convert.ToInt32(System.Console.ReadLine());
+        //     Console.WriteLine();
+        // }
+
+    }
+}
