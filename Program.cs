81f7a04d18178423ab112bf456bfc79bab6f1fcc
jiayao <jiayao3.14@outlook.com>
2019-04-15 17:19:55 +0800

    1001 A+B Format

--- Program.cs
+++ Program.cs
@@ -1,14 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PAT
 {
     class Program
     {
-
+        //1001 A+B Format
         static void Main(string[] args)
         {
-            int n = Convert.ToInt32(System.Console.ReadLine());
-            Console.WriteLine();
+            string[] input = Console.ReadLine().Split(' ');
+            int a = Convert.ToInt32(input[0]);
+            int b = Convert.ToInt32(input[1]);
+            string s = (a + b).ToString();
+            for (int i = 0; i < s.Length; i++)
+            {
+                Console.Write(s[i]);
+                if (s[i] == '-')
+                {
+                    continue;
+                }
+                if ((i + 1) % 3 == s.Length % 3 && i != s.Length - 1)
+                {
+                    Console.Write(",");
+                }
+            }
         }
         // static void Main(string[] args)
         // {
