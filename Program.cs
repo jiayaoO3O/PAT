89263c2916632a3384e2ee3c334cdce6a9d56953
jiayao <jiayao3.14@outlook.com>
2019-04-15 22:19:07 +0800

    1008 Elevator

--- Program.cs
+++ Program.cs
@@ -27,44 +27,77 @@ namespace PAT
         // }
 
         //1005 Spell It Right 
+        // static void Main(string[] args)
+        // {
+        //     //int n = Convert.ToInt32(System.Console.ReadLine());
+        //     string s = Console.ReadLine();
+        //     int sum = 0;
+        //     foreach (var num in s)
+        //     {
+        //         sum += System.Convert.ToInt32(num) - '0';//这里要注意不能直接用char转换成int,要减去48.
+        //     }
+        //     //可以用字典,也可以用数组.
+        //     // Dictionary<char, string> dict = new Dictionary<char, string>();
+        //     // dict.Add('0', "zero");
+        //     // dict.Add('1', "one");
+        //     // dict.Add('2', "two");
+        //     // dict.Add('3', "three");
+        //     // dict.Add('4', "four");
+        //     // dict.Add('5', "five");
+        //     // dict.Add('6', "six");
+        //     // dict.Add('7', "seven");
+        //     // dict.Add('8', "eight");
+        //     // dict.Add('9', "nine");
+        //     string[] dict = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+        //     s = sum.ToString();
+        //     for (int i = 0; i < s.Length; i++)
+        //     {
+        //         Console.Write(dict[s[i] - '0']);//直接读取s[i]也是char,也要减去48.
+        //         if (i != s.Length - 1)
+        //         {
+        //             Console.Write(" ");
+        //         }
+        //     }
+        //     Console.Read();
+        // }
+
+        //1008 Elevator
+        //这一题真尼玛坑,第一个N是指接下来会有的楼层的数量而不是楼层本身.
+        //所以3 2 3 1是指有3个楼层,分别是2楼,3楼,1楼.
         static void Main(string[] args)
         {
-            //int n = Convert.ToInt32(System.Console.ReadLine());
-            string s = Console.ReadLine();
-            int sum = 0;
-            foreach (var num in s)
+            string[] tokens = System.Console.ReadLine().Split();
+            int result = 0;
+            int before = 0;
+            int now = 0;
+            int[] floors = new int[int.Parse(tokens[0])];
+            for (int i = 1; i < tokens.Length; i++)
             {
-                sum += System.Convert.ToInt32(num) - '0';//这里要注意不能直接用char转换成int,要减去48.
+                floors[i - 1] = int.Parse(tokens[i]);
             }
-            //可以用字典,也可以用数组.
-            // Dictionary<char, string> dict = new Dictionary<char, string>();
-            // dict.Add('0', "zero");
-            // dict.Add('1', "one");
-            // dict.Add('2', "two");
-            // dict.Add('3', "three");
-            // dict.Add('4', "four");
-            // dict.Add('5', "five");
-            // dict.Add('6', "six");
-            // dict.Add('7', "seven");
-            // dict.Add('8', "eight");
-            // dict.Add('9', "nine");
-            string[] dict = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
-            s = sum.ToString();
-            for (int i = 0; i < s.Length; i++)
+            for (int i = 0; i < floors.Length; i++)
             {
-                Console.Write(dict[s[i] - '0']);//直接读取s[i]也是char,也要减去48.
-                if (i != s.Length - 1)
+                now = floors[i];
+                if (now > before)
                 {
-                    Console.Write(" ");
+                    result += (now - before) * 6 + 5;
+                    before = now;
+                }
+                else
+                {
+                    result += (before - now) * 4 + 5;
+                    before = now;
                 }
             }
+            Console.WriteLine(result);
             Console.Read();
         }
         // static void Main(string[] args)
         // {
-        //     int n = Convert.ToInt32(System.Console.ReadLine());
-        //     Console.WriteLine();
+        //     string[] tokens = System.Console.ReadLine().Split();
+        //     Console.Read();
         // }
+
 
 
     }
